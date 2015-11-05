356305d19365001e84f7128ea3445da5b48e9734
leedu708 <djl265@cornell.edu>
2015-11-05 15:41:50 -0500

    refactor helper module into class model

--- app/helpers/food_facility_helper.rb
+++ app/helpers/food_facility_helper.rb
@@ -1,46 +1,46 @@
 module FoodFacilityHelper
 
-  APP_TOKEN = "1KXOeqR6IrJZlerVAW1YU2Ra0"
-  API_URL = "rqzj-sfat"
+  # APP_TOKEN = "1KXOeqR6IrJZlerVAW1YU2Ra0"
+  # API_URL = "rqzj-sfat"
 
-  def input(food = "pizza", coord = [37.79, -122.39] )
+  # def input(food = "pizza", coord = [37.79, -122.39] )
 
-    client = SODA::Client.new( { :domain => "data.sfgov.org", 
-                                 :app_token => APP_TOKEN } )
+  #   client = SODA::Client.new( { :domain => "data.sfgov.org", 
+  #                                :app_token => APP_TOKEN } )
 
-    initial_lat = coord[0] - 0.01
-    end_lat = coord[0] + 0.01
-    initial_long = coord[1] - 0.01
-    end_long = coord[1] + 0.01
+  #   initial_lat = coord[0] - 0.01
+  #   end_lat = coord[0] + 0.01
+  #   initial_long = coord[1] - 0.01
+  #   end_long = coord[1] + 0.01
 
-    fooditems = food.split(", ")
-    food_query = get_food_query(fooditems)
+  #   fooditems = food.split(", ")
+  #   food_query = get_food_query(fooditems)
 
-    params = { "$where" => "latitude > '#{initial_lat}' AND latitude < '#{end_lat}' AND longitude > '#{initial_long}' AND longitude < '#{end_long}'", 
-               "$q"     => food_query,
-               "$limit" => 10 }
+  #   params = { "$where" => "latitude > '#{initial_lat}' AND latitude < '#{end_lat}' AND longitude > '#{initial_long}' AND longitude < '#{end_long}'", 
+  #              "$q"     => food_query,
+  #              "$limit" => 10 }
 
-    response = client.get(API_URL, params).select { |key, value| key.to_s.match('location') }
+  #   response = client.get(API_URL, params).select { |key, value| key.to_s.match('location') }
 
-  end
+  # end
 
-  def get_food_query(fooditems)
+  # def get_food_query(fooditems)
 
-    query = ""
+  #   query = ""
 
-    fooditems.each_with_index do |item, index|
+  #   fooditems.each_with_index do |item, index|
 
-      if index == 0
-        query = query + item
-      else
-        query = query + " OR #{item}"
-      end
+  #     if index == 0
+  #       query = query + item
+  #     else
+  #       query = query + " OR #{item}"
+  #     end
 
-    end
+  #   end
 
-    query
+  #   query
 
-  end
+  # end
 
 
 end
--- app/models/food_facility.rb
+++ app/models/food_facility.rb
@@ -29,4 +29,41 @@ class FoodFacility
   def self.q(str)
     @@client.get(ENDPOINT, '$q' => str)
   end
+
+  def self.q_user_input(food = "pizza", coord = [37.79, -122.39])
+
+    initial_lat = coord[0] - 0.01
+    end_lat = coord[0] + 0.01
+    initial_long = coord[1] - 0.01
+    end_long = coord[1] + 0.01
+
+    fooditems = food.split(", ")
+    food_query = get_food_query(fooditems)
+
+    params = { "$where" => "latitude > '#{initial_lat}' AND latitude < '#{end_lat}' AND longitude > '#{initial_long}' AND longitude < '#{end_long}'", 
+               "$q"     => food_query,
+               "$limit" => 10 }
+
+    response = @@client.get(ENDPOINT, params).select { |key, value| key.to_s.match('location') }
+
+  end
+
+  def get_food_query(fooditems)
+
+    query = ""
+
+    fooditems.each_with_index do |item, index|
+
+      if index == 0
+        query = query + item
+      else
+        query = query + " OR #{item}"
+      end
+
+    end
+
+    query
+
+  end
+
 end
